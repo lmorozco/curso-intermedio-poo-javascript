601847f5110b2e1ef3c21663b697a2bdb856f54f
lmorozco <lorozco@valizant.com>
2022-12-21 13:36:03 -0800

    Recursividad

--- main.js
+++ main.js
@@ -15,5 +15,34 @@ const obj1 = {
 // const obj3 = Object.assign({}, obj1);
 // const obj4 = Object.create(obj1);
 
-const stringifiedComplexObj = JSON.stringify(obj1);
-const obj2 = JSON.parse(stringifiedComplexObj);
+
+/*Clase 7*/
+//const stringifiedComplexObj = JSON.stringify(obj1);
+//const obj2 = JSON.parse(stringifiedComplexObj);
+
+/*Recursividad*/
+// function recursiva(){
+//     if (/* validacion */){
+//         //llamados recursivos
+//     } else {
+//         //llamados normales, sin recursividad
+//     }
+// }
+
+const numeros = [0,1,2,3,4,5,6,7,8,9,123,2342,65];
+// let numero = 0;
+// for (let i=0; i<numeros.length; i++){
+//     numero=numeros[i];
+//     console.log({i, numero});
+// }
+
+function recursiva(numbersArray){
+    if(numbersArray.length !=0){
+        const firstNumber = numbersArray[0];
+        console.log(firstNumber);
+        
+        numbersArray.shift();
+        recursiva(numbersArray);
+    }
+}
+

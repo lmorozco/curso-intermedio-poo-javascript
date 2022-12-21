28a2ffb099fbd2b744da6f349096000e9a34d140
lmorozco <lorozco@valizant.com>
2022-12-21 09:46:55 -0800

    Object.defineProperty

--- main.js
+++ main.js
@@ -12,11 +12,32 @@ console.log(Object.keys(juan));
 console.log(Object.getOwnPropertyNames(juan));
 console.log(Object.entries(juan));
 
-console.log(Object.getOwnPropertyDescriptors(juan));
+//Object.seal(juan);
+Object.freeze(juan);
 
-Object.defineProperty(juan, "pruebaNASA", {
-    value: "extraterrestres",
+Object.defineProperty(juan, "navigator", {
+    value: "Chrome",
+    enumerable: false,
     writable: true,
-    enumerable: true,
     configurable: true,
 });
+Object.defineProperty(juan, "editor", {
+    value: "VSCode",
+    enumerable: true,
+    writable: false,
+    configurable: true,
+});
+Object.defineProperty(juan, "terminal", {
+    value: "WSL",
+    enumerable: true,
+    writable: true,
+    configurable: false,
+});
+Object.defineProperty(juan, "pruebaNasa", {
+    value: "extraterrestres",
+    enumerable: false,
+    writable: false,
+    configurable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptors(juan));

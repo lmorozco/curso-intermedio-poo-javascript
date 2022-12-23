b1c77e2e674d052f1763fc0744a4f41a50cddd06
lmorozco <lorozco@valizant.com>
2022-12-23 10:26:36 -0800

    Deep Copy con Recursividad

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const obj1 = {
     c: {
         d: "d",
         e: "e",
+    },
+    editA(){
+        this.a = "AAA";
     }
 };
 
-//const obj2 = {};
+// const obj2 = {};
 // for (prop in obj1){
 //     obj2[prop] = obj1[prop];
 // }
@@ -17,17 +20,45 @@ const obj1 = {
 
 
 /*Clase 7*/
-//const stringifiedComplexObj = JSON.stringify(obj1);
-//const obj2 = JSON.parse(stringifiedComplexObj);
+// const stringifiedComplexObj = JSON.stringify(obj1);
+// const obj2 = JSON.parse(stringifiedComplexObj);
 
 /*Recursividad*/
-// function recursiva(){
-//     if (/* validacion */){
-//         //llamados recursivos
-//     } else {
-//         //llamados normales, sin recursividad
-//     }
-// }
+function isObject(subject){
+    return typeof subject == "object";
+}
+function isArray(subject){
+    return Array.isArray(subject);
+}
+
+function deepCopy(subject){
+    let copySubject;
+    
+    const subjectIsObject = isObject(subject);
+    const subjectIsArray = isArray(subject);
+
+    if (subjectIsArray){
+        copySubject = [];
+    } else if(subjectIsObject){
+        copySubject = {};
+    } else{
+        return subject;
+    }
+
+    for (key in subject){
+        const keyIsObject = isObject(subject[key]);
+
+        if (keyIsObject){
+            copySubject[key] = deepCopy(subject[key]);
+        }else if (subjectIsArray){
+            copySubject.push(subject[key]);
+        } else{
+            copySubject[key] = subject[key];
+        }
+    }
+    return copySubject;
+}
+
 
 const numeros = [0,1,2,3,4,5,6,7,8,9,123,2342,65];
 // let numero = 0;
@@ -40,7 +71,7 @@ function recursiva(numbersArray){
     if(numbersArray.length !=0){
         const firstNumber = numbersArray[0];
         console.log(firstNumber);
-        
+
         numbersArray.shift();
         recursiva(numbersArray);
     }

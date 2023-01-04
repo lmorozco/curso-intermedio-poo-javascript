cb203c0abe9769f539f8ae1a09ecc4879f19c0b4
lmorozco <lorozco@valizant.com>
2023-01-04 10:56:09 -0800

    Module pattern y namespaces

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const obj1 = {
-    a: "a",
-    b: "b",
-    c: {
-        d: "d",
-        e: "e",
-    },
-    editA(){
-        this.a = "AAA";
-    }
-};
+// const obj1 = {
+//     a: "a",
+//     b: "b",
+//     c: {
+//         d: "d",
+//         e: "e",
+//     },
+//     editA(){
+//         this.a = "AAA";
+//     }
+// };
 
 // const obj2 = {};
 // for (prop in obj1){
@@ -59,21 +59,94 @@ function deepCopy(subject){
     return copySubject;
 }
 
-
-const numeros = [0,1,2,3,4,5,6,7,8,9,123,2342,65];
-// let numero = 0;
-// for (let i=0; i<numeros.length; i++){
-//     numero=numeros[i];
-//     console.log({i, numero});
-// }
-
-function recursiva(numbersArray){
-    if(numbersArray.length !=0){
-        const firstNumber = numbersArray[0];
-        console.log(firstNumber);
-
-        numbersArray.shift();
-        recursiva(numbersArray);
-    }
+function requiredParam(param){
+    throw new Error(param + " es obligatorio");
 }
+
+function createStudent({
+    name = requiredParam("name"),
+    email = requiredParam("email"),
+    age,
+    twitter,
+    instagram,
+    facebook,
+    approvedCourses = [],
+    learningPaths = [],
+} = {}){
+    const private = {
+        "_name": name,
+
+    };
+
+    const public = {
+        email,
+        age,
+        approvedCourses,
+        learningPaths,
+        socialMedia: {
+            twitter,
+            instagram,
+            facebook,
+        },
+        readName(){
+            return private["_name"];
+        },
+        changeName(newName){
+            private["_name"] = newName;
+        },
+    };
+
+    Object.defineProperty(public, "readName", {
+        configurable: false,
+        writable: false,
+    });
+
+    return public;
+}
+
+const juan = createStudent({
+    name: "Juanito",
+    age: 18,
+    email: "juanito@frijolitos.com",
+    twitter: "fjuandc",
+});
+
+//Abstraccion sin prototipos y encapsulamiento
+// const studentBase = {
+//     name: undefined,
+//     email: undefined,
+//     age: undefined,
+//     approvedCourses: undefined,
+//     learningPaths: undefined,
+//     socialMedia: {
+//         twitter: undefined,
+//         instagram: undefined,
+//         facebook: undefined,
+//     },
+// };
+
+// const juan = deepCopy(studentBase);
+// Object.seal(juan); //Todas las propiedades no se puedan editar
+// Object.defineProperty(juan, "name", {
+//     value: "Juanito",
+//     configurable: false,
+// });
+
+
+// const numeros = [0,1,2,3,4,5,6,7,8,9,123,2342,65];
+// // let numero = 0;
+// // for (let i=0; i<numeros.length; i++){
+// //     numero=numeros[i];
+// //     console.log({i, numero});
+// // }
+
+// function recursiva(numbersArray){
+//     if(numbersArray.length !=0){
+//         const firstNumber = numbersArray[0];
+//         console.log(firstNumber);
+
+//         numbersArray.shift();
+//         recursiva(numbersArray);
+//     }
+// }
 
